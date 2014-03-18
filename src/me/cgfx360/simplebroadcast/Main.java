16c18fe1a706276573cb940c60c31851725c5661
evildude221 <piratesonlineshow@yahoo.com>
2014-03-18 15:05:03 -0700

    Initial

---
+++ src/me/cgfx360/simplebroadcast/Main.java
@@ -0,0 +1,75 @@
+package me.cgfx360.simplebroadcast;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+public final Logger logger = Logger.getLogger("Minecraft");
+	
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+	
+	}
+    
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
+		System.out.println("This plugin was requested by GetSwated!");
+		System.out.println("Report any bugs or leave any suggestions by contacting Evildude221 on Bukkit!");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		
+		
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String Label, String args[]){
+		   
+	    if(sender instanceof Player){
+	       
+	    	if (cmd.getName().equalsIgnoreCase("broadcast")){
+	       
+	            if(args.length == 0){
+	           
+	            String Error = getConfig().getString("ErrorMessage");
+	            Error = ChatColor.translateAlternateColorCodes('&', Error);	
+	            sender.sendMessage(Error);
+	           
+	            }
+	           
+	            if(args.length == 1){
+	            	if (getConfig().getBoolean("ShowServerName")){
+	            
+	            	String Server = getConfig().getString("ServerName");
+	            	Server = ChatColor.translateAlternateColorCodes('&', Server);
+	            	String Message = getConfig().getString("MessageColor");
+	            	Message = ChatColor.translateAlternateColorCodes('&', Message);
+	            	String Player = getConfig().getString("PlayerColor");
+	            	Player = ChatColor.translateAlternateColorCodes('&', Player);
+	            	Bukkit.broadcastMessage(Server + " " + Message + args[0] + Player + " ~" + sender.getName());
+	            	}else{
+	            		String Message = getConfig().getString("MessageColor");
+		            	Message = ChatColor.translateAlternateColorCodes('&', Message);
+	            	 	String Player = getConfig().getString("PlayerColor");
+		            	Player = ChatColor.translateAlternateColorCodes('&', Player);
+		            	Bukkit.broadcastMessage(Message + args[0] + Player + " ~" + sender.getName());
+	            		
+	            	
+	            	}
+	                }
+	            }
+	        }
+	    return false;
+	    }
+}
